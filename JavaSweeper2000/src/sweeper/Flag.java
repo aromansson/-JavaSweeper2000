241122ba38f7e2b99507cbf61756bc3d9d11abfe
jeweler <ggsromansson@gmail.com>
2021-08-04 00:31:33 +0300

    пофиксил время

--- JavaSweeper2000/src/sweeper/Flag.java
+++ JavaSweeper2000/src/sweeper/Flag.java
@@ -1,73 +1,96 @@
 package sweeper;
+
 /* в этом классе описывается "верхний слой" нашего сапера, где располагаются
  *флажки, отметки взорванных бомб и тому подобное 
  */
 class Flag {
-	private Matrix flagMap; //объявляем матрицу наших флагов
-	private int countOfClosedBoxes; //объявляем переменную для количества закрытых ячеек
-	
-	void start() { //метод для запуска класса
-		flagMap = new Matrix(Box.CLOSED); //инициализируем нашу матрицу с закрытыми ячейками
+
+	private Matrix flagMap; // объявляем матрицу наших флагов
+	private int countOfClosedBoxes; // объявляем переменную для количества закрытых ячеек
+	// добавляем функционал для подсчета флажков
+	public int countOfFlags; // ??объявляем переменную количества флагов
+
+	void start() { // метод для запуска класса
+		flagMap = new Matrix(Box.CLOSED); // инициализируем нашу матрицу с закрытыми ячейками
 		countOfClosedBoxes = Ranges.getSize().x * Ranges.getSize().y; // получаем размер матрицы
-	}
-	
-	Box get (Coord coord) { //геттер
-		return flagMap.get(coord); //возвращаем координату ячейки нашей матрицы
+		countOfFlags = 00; // ??инициализируем количество флагов (0)
 	}
 
-	public void setOpenedToBox(Coord coord) { //метод для открытия ячейки
-		flagMap.set(coord, Box.OPENED); //по переданным координатам устанавливаем ячейку открытой
-		countOfClosedBoxes --; //уменьшаем количество закрытых ячеек на единицу
+	Box get(Coord coord) { // геттер
+		return flagMap.get(coord); // возвращаем координату ячейки нашей матрицы
 	}
 
-	public void toggleFlagedToBox(Coord coord) { //снимаем/устанавливаем флажок
-		switch (flagMap.get(coord)) { //делаем свич в зависимости от того, что в ячейке
-	//	case FLAGED: setClosedToBox (coord); break; //если флажок - метод закрытия ячейки
-		case FLAGED: setQuestionToBox(coord); break; //????если флажок - метод cтавим вопрос
-		case INFORM :setClosedToBox(coord); break; //????если вопрос - закрываем
-		case CLOSED: setFlagedToBox(coord); break; //если открыто - метод установки флага
+	public void setOpenedToBox(Coord coord) { // метод для открытия ячейки
+		flagMap.set(coord, Box.OPENED); // по переданным координатам устанавливаем ячейку открытой
+		countOfClosedBoxes--; // уменьшаем количество закрытых ячеек на единицу
+	}
+
+	public void toggleFlagedToBox(Coord coord) { // снимаем/устанавливаем флажок
+		switch (flagMap.get(coord)) { // делаем свич в зависимости от того, что в ячейке
+		// case FLAGED: setClosedToBox (coord); break; //если флажок - метод закрытия
+		// ячейки
+		case FLAGED:
+			setQuestionToBox(coord);
+			break; // ????если флажок - метод cтавим вопрос
+		case INFORM:
+			setClosedToBox(coord);
+			break; // ????если вопрос - закрываем
+		case CLOSED:
+			setFlagedToBox(coord);
+			break; // если открыто - метод установки флага
 		}
 	}
 
-	private void setClosedToBox(Coord coord) { //метод закрытия ячейки
-		flagMap.set(coord, Box.CLOSED); //по переданным координатам закрываем ячейку
-	}
-	private void setQuestionToBox(Coord coord) { //метод для вопросительного знака
-		flagMap.set(coord, Box.INFORM); //по переданным координатам ставим вопрос
+	private void setClosedToBox(Coord coord) { // метод закрытия ячейки
+		flagMap.set(coord, Box.CLOSED); // по переданным координатам закрываем ячейку
+		countOfFlags--; // ?? обратно уменьшаем количество флагов
 	}
 
-	private void setFlagedToBox(Coord coord) { //метод для установки флажка
-		flagMap.set(coord, Box.FLAGED); //по переданным координатам ставим флажок на ячейку
+	private void setQuestionToBox(Coord coord) { // метод для вопросительного знака
+		flagMap.set(coord, Box.INFORM); // по переданным координатам ставим вопрос
+
 	}
 
-	int getCountOfClosedBoxes() { //геттер количества закрытых ячеек
+	private void setFlagedToBox(Coord coord) { // метод для установки флажка
+		flagMap.set(coord, Box.FLAGED); // по переданным координатам ставим флажок на ячейку
+		countOfFlags++; // увеличиваем значение флажков на единицу
+//		}
+	}
+
+	int getCountOfClosedBoxes() { // геттер количества закрытых ячеек
 		return countOfClosedBoxes; // возвращаем количество закрытых ячеек
 	}
 
-	void setBombedToBox(Coord coord) { //устанавливаем Бомбед на ячейку
-		flagMap.set(coord, Box.BOMBED); //по координате ставим Бомбед 
+	void setBombedToBox(Coord coord) { // устанавливаем Бомбед на ячейку
+		flagMap.set(coord, Box.BOMBED); // по координате ставим Бомбед
 	}
 
-	void setOpenedToClosedBombBox(Coord coord) { //метод открытия ячеек с бомбами
-		if (flagMap.get(coord) == Box.CLOSED) { //если ячейка закрыта
-			flagMap.set(coord, Box.OPENED); //открываем ее
+	void setOpenedToClosedBombBox(Coord coord) { // метод открытия ячеек с бомбами
+		if (flagMap.get(coord) == Box.CLOSED) { // если ячейка закрыта
+			flagMap.set(coord, Box.OPENED); // открываем ее
 		}
 	}
 
-	void setNobombToFlagedSafeBox(Coord coord) { //метод установки нобомб на ячейки с флажками 
-		if (flagMap.get(coord) == Box.FLAGED) { //если есть флажок
-			flagMap.set(coord, Box.NOBOMB); //ставим нобомб
+	void setFlagedToOpenedBombBox(Coord coord) { // метод открытия ячеек с бомбами (при выигрыше)
+		if (flagMap.get(coord) == Box.FLAGED) { // если ячейка с флажком
+			flagMap.set(coord, Box.OPENED); // открываем ее
 		}
 	}
-	
+
+	void setNobombToFlagedSafeBox(Coord coord) { // метод установки нобомб на ячейки с флажками
+		if (flagMap.get(coord) == Box.FLAGED) { // если есть флажок
+			flagMap.set(coord, Box.NOBOMB); // ставим нобомб
+		}
+	}
+
 	int getCountOfFlagedBoxesAround(Coord coord) {
-		int count = 00; //переменная количества флагов
-		for (Coord around : Ranges.getCoordsAround(coord)) { //перебираем все клетки вокруг ячейки
-			if (flagMap.get(around) == Box.FLAGED) { //если флаг есть
-				count++; //увеличиваем количество на единицу
+		int count = 00; // переменная количества флагов
+		for (Coord around : Ranges.getCoordsAround(coord)) { // перебираем все клетки вокруг ячейки
+			if (flagMap.get(around) == Box.FLAGED) { // если флаг есть
+				count++; // увеличиваем количество на единицу
 			}
 		}
-		return count;
+		return count; // возвращаем количество флагов
 	}
 
 }
--- JavaSweeper2000/src/sweeper/Game.java
+++ JavaSweeper2000/src/sweeper/Game.java
@@ -1,101 +1,138 @@
 package sweeper;
 
-public class Game { //движок игры
-	
-	private Bomb bomb; // объявляем переменную бомб 
-	private Flag flag; //объявляем переменную флагов
-	private GameState state; //объявляем переменную состояния игры
-	
-	public GameState getState() { //геттер состояния игры
-		return state; //возвращает состояние
+public class Game { // движок игры
+
+	private Bomb bomb; // объявляем переменную бомб
+	private Flag flag; // объявляем переменную флагов
+	private GameState state; // объявляем переменную состояния игры
+	private int bombsRemain; // ??объявляем переменную количества оставшихся бомб
+	private long mSeconds; // ??здесь буду хранить секунды
+
+	public GameState getState() { // геттер состояния игры
+		return state; // возвращает состояние
 
 	}
 
-	public Game(int cols, int rows, int bombs) { //конструктор принимает количество столбцов, строк и кол-ва бомб
-		Ranges.setSize(new Coord(cols, rows)); //????сеттер нашего класса
-		bomb = new Bomb(bombs); //создаем экземпляр нашего класса бомб (передаем общее количество бомб)
-		flag = new Flag(); //создаем экземпляр нашего класса флагов
+	public Game(int cols, int rows, int bombs) { // конструктор принимает количество столбцов, строк и кол-ва бомб
+		Ranges.setSize(new Coord(cols, rows)); // ????сеттер нашего класса
+		bomb = new Bomb(bombs); // создаем экземпляр нашего класса бомб (передаем общее количество бомб)
+		flag = new Flag(); // создаем экземпляр нашего класса флагов
 	}
-	
-	public void start() { //метод для старта игры
-		bomb.start(); //проинициализируем бомбы
-		flag.start(); //флаги тоже проинициализируем
-		state = GameState.PLAYED;
+
+	public void start() { // метод для старта игры
+		bomb.start(); // проинициализируем бомбы
+		flag.start(); // флаги тоже проинициализируем
+		state = GameState.PLAYED; // состояние игры - играем
+		mSeconds = System.currentTimeMillis(); // запускаем таймер
 	}
-	
-	public Box getBox(Coord coord) {//метод, который будет говорить, что изобразить в том или ином месте нашего экрана
-		if(flag.get(coord) == Box.OPENED) //делаем проверку: если во флаге находится ОПЕНЕД
-			return bomb.get(coord); //тогда возвращаем значение из матрицы бомб
+
+	public Box getBox(Coord coord) {// метод, который будет говорить, что изобразить в том или ином месте нашего
+									// экрана
+		if (flag.get(coord) == Box.OPENED) // делаем проверку: если во флаге находится ОПЕНЕД
+			return bomb.get(coord); // тогда возвращаем значение из матрицы бомб
 		else
-			return flag.get(coord); //а если нет, тогда получаем получаем координату из класса флагов
+			return flag.get(coord); // а если нет, тогда получаем получаем координату из класса флагов
 	}
 
-	public void pressLeftButton(Coord coord) { //метод для обработки клика левой кнопкой мыши
-		if(gameOver()) return; //если игра закончена - ничего не делаем
-		openBox(coord); //специальный сложный метод
-		checkWinner(); //проверяем, не выиграли ли мы
+	public void pressLeftButton(Coord coord) { // метод для обработки клика левой кнопкой мыши
+		if (gameOver())
+			return; // если игра закончена - ничего не делаем
+		openBox(coord); // специальный сложный метод
+		checkWinner(); // проверяем, не выиграли ли мы
 	}
-	
-	private void checkWinner() { //метод для проверки на победу
-		if (state == GameState.PLAYED) { //проверяем, не проиграли ли мы))
-			if (flag.getCountOfClosedBoxes() == bomb.getTotalBombs()) { //если количество закрытых клеток равно количеству бомб
-				state = GameState.WINNER; //то мы победили
+
+	private void checkWinner() { // метод для проверки на победу
+		if (state == GameState.PLAYED) { // проверяем, не проиграли ли мы))
+			if (flag.getCountOfClosedBoxes() == bomb.getTotalBombs()) { // если количество закрытых клеток равно
+																		// количеству бомб
+				for (Coord coord : Ranges.getAllCoords()) { // перебираем все координаты
+					if (bomb.get(coord) == Box.BOMB) { // если в указанной координате бомба
+						flag.setFlagedToOpenedBombBox(coord); // меняем флажки на бомбочки
+					}
+				} // открываем клетку на закрытой клетке с бомбой
+				mSeconds = (System.currentTimeMillis() - mSeconds) / 1000;
+				state = GameState.WINNER; // то мы победили
 			}
 		}
 	}
-	
-	private void openBox(Coord coord) { //метод для обработки поведения игры
-		switch (flag.get(coord)) { //перечисляем состояния флагов
-		case OPENED: setOpenedToClosedBoxesAroundNumber(coord); return; //если открыт - отерываем ячейки вокруг
-		case FLAGED: return; //если флажок - ничего не делаем
-		case CLOSED: //если закрыт - перебираем варианты
-			switch (bomb.get(coord)) { //перечисляем значения бомб
-			case ZERO: openBoxesAround (coord); return; // если открыто, то специальным методом рекурсивно открываем клетки вокруг
-			case BOMB: openBombs(coord); return; //если бомба, то вызываем метод открытия бомб
-			default : flag.setOpenedToBox (coord); return; //устанавливаем значение ячейки как открытой по координатам;
+
+	private void openBox(Coord coord) { // метод для обработки поведения игры
+		switch (flag.get(coord)) { // перечисляем состояния флагов
+		case OPENED:
+			setOpenedToClosedBoxesAroundNumber(coord);
+			return; // если открыт - отерываем ячейки вокруг
+		case FLAGED:
+			return; // если флажок - ничего не делаем
+		case CLOSED: // если закрыт - перебираем варианты
+			switch (bomb.get(coord)) { // перечисляем значения бомб
+			case ZERO:
+				openBoxesAround(coord);
+				return; // если открыто, то специальным методом рекурсивно открываем клетки вокруг
+			case BOMB:
+				openBombs(coord);
+				return; // если бомба, то вызываем метод открытия бомб
+			default:
+				flag.setOpenedToBox(coord);
+				return; // устанавливаем значение ячейки как открытой по координатам;
 			}
 		}
 	}
-	
-	private void setOpenedToClosedBoxesAroundNumber (Coord coord) {//метод, открывающий закрытые ячейки вокруг числа
+
+	private void setOpenedToClosedBoxesAroundNumber(Coord coord) {// метод, открывающий закрытые ячейки вокруг числа
 		if (bomb.get(coord) != Box.BOMB) {
 			if (flag.getCountOfFlagedBoxesAround(coord) == bomb.get(coord).getNumber()) {
-				for (Coord around : Ranges.getCoordsAround(coord)) 
-					if (flag.get(around) == Box.CLOSED) 
+				for (Coord around : Ranges.getCoordsAround(coord))
+					if (flag.get(around) == Box.CLOSED)
 						openBox(around);
 			}
 		}
 	}
-	
-	private void openBombs(Coord bombed) { //метод для открытия всех бомб
-		state = GameState.BOMBED; //устанавливаем состояние игры на бомбед
-		flag.setBombedToBox(bombed); //передаем координату, где мы взорвались
-		for (Coord coord : Ranges.getAllCoords()) { //перебираем все координаты
-			if (bomb.get(coord) == Box.BOMB) { //если в указанной координате бомба
-				flag.setOpenedToClosedBombBox (coord); //открываем клетку на закрытой клетке с бомбой
-			}
-			else {
-				flag.setNobombToFlagedSafeBox (coord); //в ином случае ставим значок НОбомб
+
+	private void openBombs(Coord bombed) { // метод для открытия всех бомб
+		state = GameState.BOMBED; // устанавливаем состояние игры на бомбед
+		flag.setBombedToBox(bombed); // передаем координату, где мы взорвались
+		for (Coord coord : Ranges.getAllCoords()) { // перебираем все координаты
+			if (bomb.get(coord) == Box.BOMB) { // если в указанной координате бомба
+				flag.setOpenedToClosedBombBox(coord); // открываем клетку на закрытой клетке с бомбой
+			} else {
+				flag.setNobombToFlagedSafeBox(coord); // в ином случае ставим значок НОбомб
 			}
 		}
+		mSeconds = (System.currentTimeMillis() - mSeconds) / 1000;
 	}
 
-	private void openBoxesAround(Coord coord) { //метод для рекурсивного открытия клеток вокруг данной клетки
-		flag.setOpenedToBox(coord); //открываем текущую клетку
-		for (Coord around : Ranges.getCoordsAround(coord)) { //перебираем все клетки вокруг
+	private void openBoxesAround(Coord coord) { // метод для рекурсивного открытия клеток вокруг данной клетки
+		flag.setOpenedToBox(coord); // открываем текущую клетку
+		for (Coord around : Ranges.getCoordsAround(coord)) { // перебираем все клетки вокруг
 			openBox(around);
 		}
 	}
 
-	public void pressRightButton(Coord coord) { //метод для обработки клика правой кнопкой мыши
-		if(gameOver()) return; //если игра закончена - ничего не делаем
-		flag.toggleFlagedToBox(coord); //при нажатии устанавливаем/снимаем флажок
+	public void pressRightButton(Coord coord) { // метод для обработки клика правой кнопкой мыши
+		if (gameOver())
+			return; // если игра закончена - ничего не делаем
+		flag.toggleFlagedToBox(coord); // при нажатии устанавливаем/снимаем флажок
 	}
 
-	private boolean gameOver() {
-		if (state == GameState.PLAYED) {
-			return false;
+	private boolean gameOver() { // метод для проверки, проиграли или нет
+		if (state == GameState.PLAYED) { // если состояние игры - играем
+			return false; // то ложно
 		}
-		return true;
+		return true; // иначе - правда
+	}
+
+	public int getCountOfFlags() { // ??геттер для количества флагов
+		return flag.countOfFlags; // ??возвращаем количество флажков
+	}
+
+	public int getBombsRemain() { // ??геттер количества оставшихся (предположительно) бомб
+		int bombRemains = bomb.getTotalBombs() - flag.countOfFlags;
+		if (bombRemains < 0)
+			bombRemains = 0;
+		return bombRemains;
+	}
+
+	public long getTime() {
+		return mSeconds;
 	}
 }
